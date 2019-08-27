2e1eef5ce553950caa7d6fe5612dad1584ec733d
mohui-22 <1179567397@qq.com>
2019-08-27 10:54:19 +0800

    first version

---
+++ bubble.js
@@ -0,0 +1,5 @@
+// $(".sound").find("a").on("mouseenter",function () {
+//    $(this).siblings().css("display","block");
+// }).on("mouseout",function () {
+//     $(this).siblings().css("display","none");
+// });
